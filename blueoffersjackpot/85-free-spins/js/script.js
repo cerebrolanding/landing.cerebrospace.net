06c2667860c0465eeab4a87c5251429b552807bd
cerebrolanding <landing@cerebrospace.net>
2026-02-12 16:30:06 +0200

    Added jackpot wheel js, html, css

---
+++ blueoffersjackpot/85-free-spins/js/script.js
@@ -0,0 +1,22 @@
+// Updated all <a> elements to dynamically set hrefs based on the TrackingToken parameter
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    var items = location.search.substr(1).split("&");
+    for (var index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    }
+
+    // Update all <a> elements with specific IDs
+    const links = document.querySelectorAll('a');
+    links.forEach(link => {
+        if (link.id === 'RegisterLink_URL') {
+            link.href = 'https://www.jackpotwheel.com/welcome/jw-280mbww-85fsww?TrackingToken=' + result;
+        }
+
+    });
+}
+
+// Call the function with the desired parameter name
+findGetParameter('TrackingToken');
